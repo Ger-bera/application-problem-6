a9413b4600b3e3484b44032d7dfe541a3385daa1
Gar-bera <1925dart@gmail.com>
2021-07-19 14:09:46 +0000

    [Add]loginページの編集まで

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end

48b97fa9a474942df706cfa070e2abf6326c1310
Gar-bera <1925dart@gmail.com>
2021-07-26 06:25:57 +0000

    [D]

---
+++ db/migrate/20210726062015_add_reset_password_token_to_users.rb
@@ -0,0 +1,5 @@
+class AddResetPasswordTokenToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, :reset_password_token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_082258) do
+ActiveRecord::Schema.define(version: 2021_07_26_062015) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_07_20_082258) do
     t.string "profile_image_id"
     t.string "reset_password_token"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

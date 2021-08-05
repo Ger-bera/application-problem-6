be0280e8c6f8db7a35030944a41435f6dcb343fb
Ger-bera <1925dart@gmail.com>
2021-08-05 12:34:49 +0000

    [Finish]応用課題４

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @newbook = Book.new
+    @users = User.all
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,24 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comment, dependent: :destroy
 
+  has_many :do_follows, class_name: "Relationship", foreign_key: "followed_id",dependent: :destroy
+  has_many :followers, through: :do_follows, source: :follower
+
+  has_many :done_follows, class_name: "Relationship", foreign_key: "follower_id",dependent: :destroy
+  has_many :followings, through: :done_follows, source: :followed
+
+  def follow(user_id)
+    done_follows.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    done_follows.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
   attachment :profile_image
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
